d489f9b7ae0dfadac9b0840b342b9dda048c7ed3
Viktor Vorobev <vorobev@in.tum.de>
2022-05-04 14:38:08 +0200

    [NRRPLT-8533] Fixes

--- .ci/cmake_cache/nest-gazebo-nomqtt-noros.cmake
+++ .ci/cmake_cache/nest-gazebo-nomqtt-noros.cmake
@@ -2,8 +2,8 @@ set(COVERAGE ON CACHE BOOL "Evaluate coverage")
 set(BUILD_RST OFF CACHE BOOL "Build rst files for the documentation")
 
 
-set(ENABLE_GAZEBO OFF CACHE "Enable gazebo support")
-set(ENABLE_NEST OFF CACHE "Enable nest support")
+set(ENABLE_GAZEBO FULL CACHE "Enable gazebo support")
+set(ENABLE_NEST FULL CACHE "Enable nest support")
 
 
 set(ENABLE_ROS OFF CACHE "Enable ROS support")
